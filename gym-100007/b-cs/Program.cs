e6ad7b6a11aefac413c78c93922bac69db534a11
Evgeniy A. Simonenko <easimonenko@mail.ru>
2016-10-08 23:36:15 +0300

    Solution for B in Gym 100007.

---
+++ gym-100007/b-cs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace bcs
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			StreamReader reader;
+			StreamWriter writer;
+			#if (ONLINE_JUDGE)
+			reader = new StreamReader("input.txt");
+			writer = new StreamWriter("output.txt");
+			#else
+			reader = new StreamReader(Console.OpenStandardInput());
+			writer = new StreamWriter(Console.OpenStandardOutput());
+			#endif
+			long n = Int64.Parse(reader.ReadLine());
+			int counter = 0;
+			long k = 2;
+			while (k * k * k * k <= n) {
+				int c = 0;
+				for (int d = 2; d <= k; d++) {
+					if (k % d == 0) {
+						c += 1;
+					}
+					if (c > 1) {
+						break;
+					}
+				}
+				if (c == 1) {
+					counter++;
+				}
+				k++;
+			}
+			writer.WriteLine(counter);
+
+			writer.Close();
+		}
+	}
+}
