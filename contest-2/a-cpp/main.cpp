482c4ea980c2d0b851bf75972223d9f47e70c649
Evgeniy A. Simonenko <easimonenko@mail.ru>
2016-11-15 23:31:20 +0300

    Solution for A in Contest 2.

---
+++ contest-2/a-cpp/main.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+#ifdef __DEBUG__
+    //freopen("A_1.in", "r", stdin);
+#endif
+    int n;
+    cin >> n;
+    list<pair<string,int> > records;
+    for (int i = 0; i < n; i++) {
+        static string name;
+        static int score;
+        cin >> name >> score;
+        records.push_back(pair<string,int>(name,score));
+    }
+    map<string,int> scores;
+    for (list<pair<string,int> >::iterator it = records.begin(); it != records.end(); it++) {
+        scores[it->first] += it->second;
+    }
+    int m = scores.begin()->second;
+    set<string> names;
+    names.insert(scores.begin()->first);
+    for (map<string,int>::iterator it = scores.begin(); it != scores.end(); it++) {
+        if (it->second == m) {
+            names.insert(it->first);
+            continue;
+        }
+        if (it->second > m) {
+            m = it->second;
+            names.clear();
+            names.insert(it->first);
+        }
+    }
+    
+    if (names.size() == 1) {
+        cout << *(names.begin()) << endl;
+    } else {
+        // вновь моделируем игру
+        map<string,int> scores;
+        for (list<pair<string,int> >::iterator it = records.begin(); it != records.end(); it++) {
+            static string name;
+            name = it->first;
+            scores[name] += it->second;
+            if (scores[name] >= m && names.find(name) != names.end()) {
+                cout << name << endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
--- contest-2/a-cs/Program.cs
+++ contest-2/a-cs/Program.cs
@@ -1,7 +1,5 @@
-﻿// Program.cs
-// Author: Evgeniy A. Simonenko <easimonenko@mail.ru>
-// Copyright (c) 2016 Evgeniy A. Simonenko <easimonenko@mail.ru>
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace acs
 {
@@ -9,7 +7,30 @@ namespace acs
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var n = Int32.Parse(Console.ReadLine());
+
+			var gamers = new Dictionary<string, int>();
+			var input = Console.ReadLine().Split();
+			var winner_name = input[0];
+			var winner_score = Int32.Parse(input[1]);
+			gamers[winner_name] = winner_score;
+			for (int i = 1; i < n; i++) {
+				input = Console.ReadLine().Split();
+				var current_name = input[0];
+				var current_score = Int32.Parse(input[1]);
+				if (gamers.ContainsKey(current_name)) {
+					gamers[current_name] += current_score;
+				}
+				else {
+					gamers.Add(current_name, current_score);
+				}
+				if (gamers[current_name] > winner_score) {
+					winner_score = gamers[current_name];
+					winner_name = current_name;
+				}
+			}
+
+			Console.WriteLine(winner_name);
 		}
 	}
 }
