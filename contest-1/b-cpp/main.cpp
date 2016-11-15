513481a4f8acddf93da63bde3da998e31ccd957c
Evgeniy A. Simonenko <easimonenko@mail.ru>
2016-11-15 23:30:20 +0300

    Solutions for B, C in Contest 1.

---
+++ contest-1/b-cpp/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Coord
+{
+	int row;
+	int column;
+	Coord(string input) {
+		if (input[0] == 'R' && isdigit(input[1])) {
+			int idxOfC = -1;
+
+		}
+	}
+};
+
+int main (int argc, char *argv[])
+{
+	int k;
+	cin >> k;
+	while (k > 0) {
+		string input;
+		Coord coord(input);
+		k--;
+	}
+	
+	return 0;
+}
+
---
+++ contest-1/c-cs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ccs
+{
+	struct Pair {
+		public double x;
+		public double y;
+		public Pair(double x, double y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var coords = new Pair[3];
+			for (int i = 0; i < 3; i++) {
+				var input = Console.ReadLine().Split();
+				coords[i] = new Pair(Double.Parse(input[0]), Double.Parse(input[1]));
+			}
+
+
+		}
+	}
+}
